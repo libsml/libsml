ca4e9f2dba1046c2b860ef3117b8481ed788160c
huangfish <huang_yuu330122>
2015-10-10 22:27:31 +0800

    rm libsml-lbfgs

--- libsml-lbfgs/src/main/java/com/github/libsml/lbfgs/function/VecFreeFunction.java
+++
@@ -1,9 +0,0 @@
-package com.github.libsml.lbfgs.function;
-
-public interface VecFreeFunction {
-
-	void vecsDot(float[][] b, int k, float[] x, float[] xp, float[] g,
-				 float[] gp, float[] pg);
-
-	void computeDirect(float[] d, float[] theta, int k);
-}
